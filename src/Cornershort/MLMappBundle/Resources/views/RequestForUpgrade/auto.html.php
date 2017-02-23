44e11e82e49b0813e43e771b8a7acc063d881c03
johnjoseph23 <johnjosephhilario23@gmail.com>
2017-02-23 17:19:08 +0800

    Joseph: Update Request for Upgrade Page and create sub menus for Auto and Manual pages.

---
+++ src/Cornershort/MLMappBundle/Resources/views/RequestForUpgrade/auto.html.php
@@ -0,0 +1,7 @@
+<?php $view->extend('CornershortMLMappBundle:Layout:layout.html.php') ?>
+
+<?php $view['slots']->start('content') ?>
+    <div class="note note-info">
+        <p>Request For Auto Upgrade Page</p>
+    </div>
+<?php $view['slots']->stop() ?>
---
+++ src/Cornershort/MLMappBundle/Resources/views/RequestForUpgrade/manual.html.php
@@ -0,0 +1,56 @@
+<?php $view->extend('CornershortMLMappBundle:Layout:layout.html.php') ?>
+
+<?php $view['slots']->start('content') ?>
+    <div class="row">
+        <div class="col-md-12">
+            <!-- BEGIN Portlet PORTLET-->
+            <div class="portlet-body">
+                <div class="row">
+                    <div class="col-md-12">
+                        <!-- BEGIN Portlet PORTLET-->
+                        <div class="portlet box blue">
+                            <div class="portlet-title">
+                                <div class="caption">
+                                    <i class="fa fa-gift"></i>Your Next Leader To Contact For Upgrade</div>
+                                <div class="tools">
+                                    <a href="javascript:;" class="collapse"> </a>
+                                    <a href="#portlet-config" data-toggle="modal" class="config"> </a>
+                                    <a href="javascript:;" class="reload"> </a>
+                                    <a href="" class="fullscreen"> </a>
+                                    <a href="javascript:;" class="remove"> </a>
+                                </div>
+                            </div>
+                            <div class="portlet-body">
+                                <div class="scroller" style="height:200px; display: flex; justify-content: center; align-items: center;">
+                                    <table>
+                                        <tr>
+                                            <th style="font-size:2vw;">Leader's ID:&nbsp</th>
+                                            <th style="font-size:2vw; font-weight: normal;">PH000002</th>
+                                        </tr>
+                                        <tr>
+                                            <th style="font-size:2vw;">Contact Number:&nbsp</th>
+                                            <th style="font-size:2vw; font-weight: normal;">0925 123 4567</th>
+                                        </tr>
+                                        <tr>
+                                            <th style="font-size:2vw;">Address:&nbsp</th>
+                                            <th style="font-size:2vw; font-weight: normal;">Clark, Pampanga</th>
+                                        </tr>
+                                    </table>
+                                </div>
+                                <div class="scroller" style="height:200px; display: flex; justify-content: center; align-items: center;">
+                                    <h2 style="font-weight:bold; color:red; font-family:Source Sans Pro;">CLICK THE BUTTON TO REQUEST FOR AN UPGRADE</h2>
+                                </div>
+                                <div class="form-actions noborder" style="display:flex; justify-content:flex-end;">
+                                    <button type="button" id="visor-form-submit" class="btn blue">Upgrade Now</button>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- END Portlet PORTLET-->
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+<?php $view['slots']->stop() ?>
