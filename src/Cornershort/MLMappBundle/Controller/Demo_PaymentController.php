3e08d54795295cad868a9c36cf3742dbf0515a0f
paulo <paulo.avila@orangeandbronze.com.au>
2017-02-28 07:29:48 +0800

    Continuation of #25

--- src/Cornershort/MLMappBundle/Controller/Demo_PaymentController.php
+++ src/Cornershort/MLMappBundle/Controller/Demo_PaymentController.php
@@ -14,6 +14,20 @@ use Cornershort\MLMappBundle\Entity\Order;
 
 class Demo_PaymentController extends Controller {
 
+    private function createPayment($order){
+        $instruction = $order->getPaymentInstruction();
+        $pendingTransaction = $instruction->getPendingTransaction();
+
+        if ($pendingTransaction !== null) {
+            return $pendingTransaction->getPayment();
+        }
+
+        $ppc = $this->get('payment.plugin_controller');
+        $amount = $instruction->getAmount() - $instruction->getDepositedAmount();
+
+        return $ppc->createPayment($instruction->getId(), $amount);
+    }
+
     public function newAction(){
         $membership_amount = '1500';
         $em = $this->getDoctrine()->getManager();
@@ -21,7 +35,7 @@ class Demo_PaymentController extends Controller {
         $em->persist($order);
         $em->flush();
 
-        return $this->redirect($this->generateUrl('cornershort_mlmapp_demo_payment_page_view', [
+        return $this->redirect($this->generateUrl('cornershort_mlmapp_demo_payment_page_show', [
             'order' => $order->getId()
         ]));
     }
@@ -38,6 +52,55 @@ class Demo_PaymentController extends Controller {
             'order' => $order,
             'form'  => $form->createView(),
         ]);
+    }
+
+    public function showAction(Request $request, Order $order){
+
+        $form = $this->createForm('jms_choose_payment_method', null, [
+            'amount'   => $order->getAmount(),
+            'currency' => 'PHP',
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ppc = $this->get('payment.plugin_controller');
+            $ppc->createPaymentInstruction($instruction = $form->getData());
+
+            $order->setPaymentInstruction($instruction);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($order);
+            $em->flush($order);
+
+            return $this->redirect($this->generateUrl('cornershort_mlmapp_demo_payment_page_create', [
+                'order' => $order->getId(),
+            ]));
+        }
+
+        return $this->render('CornershortMLMappBundle:Demo_Payment:view.html.twig', [
+            'order' => $order,
+            'form'  => $form->createView(),
+        ]);
+    }
+
+    public function paymentCreateAction(Order $order){
+        $payment = $this->createPayment($order);
+
+        $ppc = $this->get('payment.plugin_controller');
+        $result = $ppc->approveAndDeposit($payment->getId(), $payment->getTargetAmount());
+
+        if ($result->getStatus() === Result::STATUS_SUCCESS) {
+            return $this->redirect($this->generateUrl('cornershort_mlmapp_demo_payment_page_complete', [
+                'order' => $order->getId(),
+            ]));
+        }
+
+        throw $result->getPluginException();
+
+        // In a real-world application you wouldn't throw the exception. You would,
+        // for example, redirect to the showAction with a flash message informing
+        // the user that the payment was not successful.
     }
 
 }
---
+++ src/Cornershort/MLMappBundle/Resources/views/Demo_Payment/payment-complete.html.php
@@ -0,0 +1,13 @@
+
+<?php $view->extend('CornershortMLMappBundle:Layout:layout.html.php') ?>
+
+<?php $view['slots']->set('PageTitle', 'Payment Successful!') ?>
+<?php $view['slots']->set('PageTitleSmall', '') ?>
+
+<?php $view['slots']->start('content') ?>
+<div class="tab-content">
+    <div class="pane">
+
+    </div>
+</div>
+<?php $view['slots']->stop() ?>
