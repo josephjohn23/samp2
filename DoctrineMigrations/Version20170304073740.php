1dcb27ceaf9613c5f81b74f26eee04e5a990742b
johnjoseph23 <johnjosephhilario23@gmail.com>
2017-03-05 15:12:43 +0800

    Joseph: Add migration files.

---
+++ DoctrineMigrations/Version20170304073740.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170304073740 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE member_payment_history CHANGE is_level_paid is_level_paid INT NOT NULL, CHANGE is_product_paid is_product_paid INT NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE member_payment_history CHANGE is_level_paid is_level_paid TINYINT(1) NOT NULL, CHANGE is_product_paid is_product_paid TINYINT(1) NOT NULL');
+    }
+}
